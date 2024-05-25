23dc6151f2e16d4f1f8d61779ff1530ae2a191d8
Ethan Deatrick <deatro66@gmail.com>
2024-05-24 22:27:24 -0400

    Updated inline documentation

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ package NumberGuessingGame;
 
 public class Main {
     
-
     public static void main(String[] args) {
+        // Start game - Create main menu JFrame
         new MenuFrame();
     }
 
